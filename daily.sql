554386da005767718794eb5c97aa50987dc3e1e1
RozenieEsparagoza <145411735+RozenieEsparagoza@users.noreply.github.com>
2024-01-09 13:52:27 +0800

    Add files via upload

---
+++ daily.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 09, 2024 at 06:51 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `daily`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daily`
+--
+
+CREATE TABLE `daily` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `daily`
+--
+
+INSERT INTO `daily` (`id`, `title`) VALUES
+(1, 'gym-5am'),
+(3, 'tiktok-10pm'),
+(4, 'eating breakfast-7am');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_fname` varchar(255) NOT NULL,
+  `user_Iname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_pass` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_fname`, `user_Iname`, `user_email`, `user_pass`) VALUES
+(1, 'roz', 'nie', 'roznie@gmail.com', '$2y$10$WeTkp3GTxFXpRfmqoBNL/.3mDU5JrHtvvcUGPccLWmQg4OowCVDIO'),
+(2, 'roi', 'roz', 'rozroz@gmail.com', '$2y$10$eTmdel5OqwnADTGw/E8mNeFbik.71viBVpFOmDlPRHP2ibGOm3DY6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `daily`
+--
+ALTER TABLE `daily`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `daily`
+--
+ALTER TABLE `daily`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
